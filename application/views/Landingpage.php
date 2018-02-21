29f2d9cb7e3f58a2b1663a2d7498cd8b0e69f74a
Candraibra <canisanie@gmail.com>
2018-02-21 13:06:31 +0700

    'message'

---
+++ application/views/Landingpage.php
@@ -0,0 +1,140 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+  <head>
+	   <meta charset="utf-8">
+	   <title>Landing Page Goat</title>
+	   <link href='src/img/logo.png' rel='shortcut icon'>
+	   <link rel ="stylesheet" href="src/css/bootstrap.min.css">
+  </head>
+  <body  data-spy="scroll" data-target=".navbar" data-offset="50">
+      <nav class="navbar navbar-expand-sm bg-dark navbar-dark fixed-top" id="myNavbar">
+          <a class="navbar-brand"><img src="src/img/logo.png" alt="Dispute Bills"></a>
+          <ul class="navbar-nav">
+              <li class="nav-item">
+                  <a class="nav-link" href="#section2">Home</a>
+              </li>
+              <li class="nav-item">
+                  <a class="nav-link" href="#section3">About</a>
+              </li>
+              <!-- Dropdown -->
+              <li class="nav-item dropdown">
+                  <a class="nav-link dropdown-toggle" href="#" id="navbardrop" data-toggle="dropdown">More</a>
+                  <div class="dropdown-menu">
+                      <a class="dropdown-item" href="#">Teknologi</a>
+                      <a class="dropdown-item" href="#">Otomotif</a>
+                      <a class="dropdown-item" href="#">Pendidikan</a>
+                  </div>
+              </li>
+          </ul>
+      </nav>
+      <div class="container">
+          <div id="section2"  class="jumbotron" style="padding-top:70px;padding-bottom:70px">
+              <h1>Ahok Bawa 12 Bukti ke Sidang Cerai, Ada Chat Veronica dan JT</h1>
+              <p>Liputan6.com, Jakarta Sidang perceraian mantan Gubernur DKI Jakarta Basuki Tjahaja Purnama atau Ahok dengan istrinya, Veronica Tan, memasuki babak baru. Ahok melalui kuasa hukumnya menyerahkan bukti perselingkuhan.</p>
+              <button style="margin-bottom:20px" data-toggle="collapse" data-target="#demo" class="btn btn-secondary">Selengkapnya</button>
+              <div id="demo" class="collapse">
+                  <img class="mx-auto d-block" src="https://cdn0-a.production.images.static6.com/0SxA4MCHeGEEQkRS07ZrXwHa83A=/640x360/smart/filters:quality(75):strip_icc():format(webp)/liputan6-media-production/medias/1878854/original/013811400_1518088034-041366600_1515468782-Ahok.jpg" alt="Girl in a jacket">
+                  <p>Liputan6.com, Jakarta Sidang perceraian mantan Gubernur DKI Jakarta Basuki Tjahaja Purnama atau Ahok dengan istrinya, Veronica Tan, memasuki babak baru. Ahok melalui kuasa hukumnya menyerahkan bukti perselingkuhan.</p>
+
+                  <p>"Kami serahkan bukti yang mendukung gugatan termasuk akta kelahiran dan surat-surat," ujar kuasa hukum Ahok, Josefina Agatha Syukur, Rabu (21/2/2018).
+                  Ahok berencana menerbitkan buku. Hal itu disampaikan Kuasa Hukum yang juga adik Ahok, Fifi Lety Indra di Pengadilan Jakarta Utara, di sela sidang perceraian Ahok dan Veronica Tan.</p>
+
+                  <p>Dalam buku itu, Ahok akan menungkan kisah-kisah yang dialaminya selama berada di Rumah Tahanan (Rutan) Mako Brimob Kelapa II Depok. Perceraiannya dengan Veronica Tan, menurut Fifi, juga akan dibeberkan di sana.</p>
+
+                  <p>"Keputusan (perceraiannya) ini sangat sulit. Ahok sempat bergumam malam-malam. Sebelum memutuskan ini dia membaca berulang kali firman Tuhan," ucap Fifi, Rabu (14/2/2018)</p>
+
+                  <p>Ia menambahkan, buku kemungkinan akan berjudul Renungan 360 hari di balik jeruji besi. "Nanti dibeli saja deh," kata Fifi setengah berseloroh.</p>
+
+                  <p>Sejauh ini, Ahok sudah menyusun hingga 200 lembar. "Per harinya 1 lembar," ujar dia.</p>
+
+                  <p>Selain menulis buku, Ahok juga sering membaca Alkitab. Bahkan, Ahok sudah menguasai dua Alkitab berbahasa asing.</p>
+
+                  <p>"Dia sudah hafal Alkitab Bahasa Inggris dan Mandarin," Fifi menandaskan.</p>
+              </div>
+          </div>
+          <div id="section3" class="jumbotron" style="padding-top:70px;padding-bottom:70px">
+                <h1>Ahok Bawa 12 Bukti ke Sidang Cerai, Ada Chat Veronica dan JT</h1>
+              <p>Liputan6.com, Jakarta Sidang perceraian mantan Gubernur DKI Jakarta Basuki Tjahaja Purnama atau Ahok dengan istrinya, Veronica Tan, memasuki babak baru. Ahok melalui kuasa hukumnya menyerahkan bukti perselingkuhan.</p>
+              <button style="margin-bottom:20px" data-toggle="collapse" data-target="#demo1" class="btn btn-secondary">Selengkapnya</button>
+              <div id="demo1" class="collapse">
+                  <img class="mx-auto d-block" src="https://cdn0-a.production.images.static6.com/0SxA4MCHeGEEQkRS07ZrXwHa83A=/640x360/smart/filters:quality(75):strip_icc():format(webp)/liputan6-media-production/medias/1878854/original/013811400_1518088034-041366600_1515468782-Ahok.jpg" alt="Girl in a jacket">
+                  <p>Liputan6.com, Jakarta Sidang perceraian mantan Gubernur DKI Jakarta Basuki Tjahaja Purnama atau Ahok dengan istrinya, Veronica Tan, memasuki babak baru. Ahok melalui kuasa hukumnya menyerahkan bukti perselingkuhan.</p>
+
+                  <p>"Kami serahkan bukti yang mendukung gugatan termasuk akta kelahiran dan surat-surat," ujar kuasa hukum Ahok, Josefina Agatha Syukur, Rabu (21/2/2018).
+                  Ahok berencana menerbitkan buku. Hal itu disampaikan Kuasa Hukum yang juga adik Ahok, Fifi Lety Indra di Pengadilan Jakarta Utara, di sela sidang perceraian Ahok dan Veronica Tan.</p>
+
+                  <p>Dalam buku itu, Ahok akan menungkan kisah-kisah yang dialaminya selama berada di Rumah Tahanan (Rutan) Mako Brimob Kelapa II Depok. Perceraiannya dengan Veronica Tan, menurut Fifi, juga akan dibeberkan di sana.</p>
+
+                  <p>"Keputusan (perceraiannya) ini sangat sulit. Ahok sempat bergumam malam-malam. Sebelum memutuskan ini dia membaca berulang kali firman Tuhan," ucap Fifi, Rabu (14/2/2018)</p>
+
+                  <p>Ia menambahkan, buku kemungkinan akan berjudul Renungan 360 hari di balik jeruji besi. "Nanti dibeli saja deh," kata Fifi setengah berseloroh.</p>
+
+                  <p>Sejauh ini, Ahok sudah menyusun hingga 200 lembar. "Per harinya 1 lembar," ujar dia.</p>
+
+                  <p>Selain menulis buku, Ahok juga sering membaca Alkitab. Bahkan, Ahok sudah menguasai dua Alkitab berbahasa asing.</p>
+
+                  <p>"Dia sudah hafal Alkitab Bahasa Inggris dan Mandarin," Fifi menandaskan.</p>
+              </div>
+          </div>
+          <div id="section41" class="jumbotron" style="padding-top:70px;padding-bottom:70px">
+                <h1>Ahok Bawa 12 Bukti ke Sidang Cerai, Ada Chat Veronica dan JT</h1>
+              <p>Liputan6.com, Jakarta Sidang perceraian mantan Gubernur DKI Jakarta Basuki Tjahaja Purnama atau Ahok dengan istrinya, Veronica Tan, memasuki babak baru. Ahok melalui kuasa hukumnya menyerahkan bukti perselingkuhan.</p>
+              <button style="margin-bottom:20px" data-toggle="collapse" data-target="#demo2" class="btn btn-secondary">Selengkapnya</button>
+              <div id="demo2" class="collapse">
+                  <img class="mx-auto d-block" src="https://cdn0-a.production.images.static6.com/0SxA4MCHeGEEQkRS07ZrXwHa83A=/640x360/smart/filters:quality(75):strip_icc():format(webp)/liputan6-media-production/medias/1878854/original/013811400_1518088034-041366600_1515468782-Ahok.jpg" alt="Girl in a jacket">
+                  <p>Liputan6.com, Jakarta Sidang perceraian mantan Gubernur DKI Jakarta Basuki Tjahaja Purnama atau Ahok dengan istrinya, Veronica Tan, memasuki babak baru. Ahok melalui kuasa hukumnya menyerahkan bukti perselingkuhan.</p>
+
+                  <p>"Kami serahkan bukti yang mendukung gugatan termasuk akta kelahiran dan surat-surat," ujar kuasa hukum Ahok, Josefina Agatha Syukur, Rabu (21/2/2018).
+                  Ahok berencana menerbitkan buku. Hal itu disampaikan Kuasa Hukum yang juga adik Ahok, Fifi Lety Indra di Pengadilan Jakarta Utara, di sela sidang perceraian Ahok dan Veronica Tan.</p>
+
+                  <p>Dalam buku itu, Ahok akan menungkan kisah-kisah yang dialaminya selama berada di Rumah Tahanan (Rutan) Mako Brimob Kelapa II Depok. Perceraiannya dengan Veronica Tan, menurut Fifi, juga akan dibeberkan di sana.</p>
+
+                  <p>"Keputusan (perceraiannya) ini sangat sulit. Ahok sempat bergumam malam-malam. Sebelum memutuskan ini dia membaca berulang kali firman Tuhan," ucap Fifi, Rabu (14/2/2018)</p>
+
+                  <p>Ia menambahkan, buku kemungkinan akan berjudul Renungan 360 hari di balik jeruji besi. "Nanti dibeli saja deh," kata Fifi setengah berseloroh.</p>
+
+                  <p>Sejauh ini, Ahok sudah menyusun hingga 200 lembar. "Per harinya 1 lembar," ujar dia.</p>
+
+                  <p>Selain menulis buku, Ahok juga sering membaca Alkitab. Bahkan, Ahok sudah menguasai dua Alkitab berbahasa asing.</p>
+
+                  <p>"Dia sudah hafal Alkitab Bahasa Inggris dan Mandarin," Fifi menandaskan.</p>
+              </div>
+          </div>
+      </div>
+    ``<script src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+      <script src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+      <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+      <script>
+          $(document).ready(function(){
+            // Add scrollspy to <body>
+            $('body').scrollspy({target: ".navbar", offset: 50});   
+
+            // Add smooth scrolling on all links inside the navbar
+            $("#myNavbar a").on('click', function(event) {
+              // Make sure this.hash has a value before overriding default behavior
+              if (this.hash !== "") {
+                // Prevent default anchor click behavior
+                event.preventDefault();
+
+                // Store hash
+                var hash = this.hash;
+
+                // Using jQuery's animate() method to add smooth page scroll
+                // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+                $('html, body').animate({
+                  scrollTop: $(hash).offset().top
+                }, 400, function(){
+             
+                  // Add hash (#) to URL when done scrolling (default click behavior)
+                  window.location.hash = hash;
+                });
+              }  // End if
+            });
+          });
+      </script> 
+  </body>
+</html>
